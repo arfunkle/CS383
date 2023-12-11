45548cfbe2f01ba3b288ae292441c7cf73a0ad70
mushrom81 <44351080+mushrom81@users.noreply.github.com>
2023-12-11 13:05:12 -0800

    Signatures for the Dawg class

---
+++ src/scrabble/Dawg.java
@@ -0,0 +1,11 @@
+package scrabble;
+
+public class Dawg {
+  // Give the file path to words.txt
+  public Dawg(String file) {
+  }
+
+  // Given a board state and a (player's) hand, return all possible legal moves that can be made
+  public ArrayList<PlayWord> findAllMoves(Board board, int playerNum) {
+  }
+}
